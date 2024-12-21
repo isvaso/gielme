6c8b39fc94695e053bfb1447d847a62d0d3f1575
Ilya Biryukov <is.vaso@gmail.com>
2024-12-21 19:09:51 +0600

    Create base FileManager

---
+++ src/main/java/com/isvaso/io/FileManager.java
@@ -0,0 +1,28 @@
+package com.isvaso.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileManager {
+
+    public List<String> read(String filePath) {
+        try {
+            Path path = Path.of(filePath);
+            return Files.readAllLines(path);
+        } catch (IOException exception) {
+            throw new RuntimeException("Error while reading file '%s'".formatted(filePath));
+        }
+    }
+
+    public boolean write(String filePath, String data) {
+        try {
+            Path path = Path.of(filePath);
+            Files.writeString(path, data);
+        } catch (IOException exception) {
+            throw new RuntimeException("Error while writing file '%s'".formatted(filePath));
+        }
+        return true;
+    }
+}
