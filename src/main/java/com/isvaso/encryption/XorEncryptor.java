d6fdfe01ece23fa0bc51c66de83cfd735d9a7d0b
Ilya Biryukov <is.vaso@gmail.com>
2025-12-05 01:33:07 +0600

    Refactoring some

--- src/main/java/com/isvaso/encryption/XorEncryptor.java
+++ src/main/java/com/isvaso/encryption/XorEncryptor.java
@@ -14,15 +14,12 @@ public class XorEncryptor implements Encryptor {
         return process(data);
     }
 
-    // TODO: delete {} in if/for
     private String process(String data) {
-        if(data == null || data.isEmpty()) {
+        if(data == null || data.isEmpty())
             return data;
-        }
         char[] result = new char[data.length()];
-        for (int i = 0; i < data.length(); i++) {
+        for (int i = 0; i < data.length(); i++)
             result[i] = (char) (data.charAt(i) ^ ENCRYPTION_KEY.charAt(i % ENCRYPTION_KEY.length()));
-        }
         return new String(result);
     }
 }
