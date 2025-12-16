18804b092116b811e1d9651dbd00159e54197256
Ilya Biryukov <is.vaso@gmail.com>
2025-12-16 22:10:29 +0600

    Modify TaskSerializer and SerializationProperties

--- src/main/java/com/isvaso/serialization/SerializationProperties.java
+++ src/main/java/com/isvaso/serialization/SerializationProperties.java
@@ -3,5 +3,7 @@ package com.isvaso.serialization;
 // TODO: non static for testing
 public class SerializationProperties {
 
-    public static final String DELIMITER = "/";
+    public static final String FIELD_DELIMITER = "/";
+
+    public static final String ELEMENT_DELIMITER = "\n";
 }
