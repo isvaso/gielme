5417519984b34daf8a7a5105e4687515c31aaade
Ilya Biryukov <78846548+isvaso@users.noreply.github.com>
2025-12-16 20:36:02 +0600

    Test/data version serilizer (#21)
    * Add DataVersion validation in DataVersionSerializer.serialize() method
    
    * Add isBlankOrNull() method to StringValidator
    
    * Add catch NumberFormatException and validation to DataVersionSerializer.deserialize() method
    
    * Add tests for DataVersionSerializer serialize() and deserialize() methods

--- src/main/java/com/isvaso/serialization/DataVersionSerializer.java
+++ src/main/java/com/isvaso/serialization/DataVersionSerializer.java
@@ -11,13 +11,23 @@ import java.util.Optional;
 public class DataVersionSerializer {
 
     public String serialize(DataVersion dataVersion) throws SerializerException {
+        if (dataVersion == null)
+            throw new SerializerException("DataVersion cannot be null");
+        if (dataVersion.getVersion() < 0)
+            throw new SerializerException("Version should be positive, but now is ''%s'".formatted(dataVersion.getVersion()));
         return String.valueOf(dataVersion.getVersion());
     }
 
     public Optional<DataVersion> deserialize(String string) throws SerializerException {
-        if (StringValidator.isEmptyOrNull(string))
-            throw new SerializerException("Invalid string with data version");
-        int version = Integer.parseInt(string);
-        return Optional.of(new DataVersion(version));
+        if (StringValidator.isBlankOrNull(string))
+            throw new SerializerException("Invalid string with data version '%s'".formatted(string));
+        try {
+            int version = Integer.parseInt(string);
+            if (version < 0)
+                throw new SerializerException("Version should be positive, but now is '%s'".formatted(version));
+            return Optional.of(new DataVersion(version));
+        } catch (NumberFormatException exception) {
+            throw new SerializerException("Error while parsing data version '%s'".formatted(string), exception);
+        }
     }
 }
--- src/main/java/com/isvaso/util/StringValidator.java
+++ src/main/java/com/isvaso/util/StringValidator.java
@@ -5,4 +5,8 @@ public class StringValidator {
     public static boolean isEmptyOrNull(String string) {
         return string == null || string.isEmpty();
     }
+
+    public static boolean isBlankOrNull(String string) {
+        return string == null || string.isBlank();
+    }
 }
