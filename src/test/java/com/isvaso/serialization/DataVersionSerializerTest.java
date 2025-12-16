ccc45155c304bfed29db350f0bdbc716c56133cd
Ilya Biryukov <is.vaso@gmail.com>
2025-12-16 20:35:21 +0600

    Add tests for DataVersionSerializer serialize() and deserialize() methods

---
+++ src/test/java/com/isvaso/serialization/DataVersionSerializerTest.java
@@ -0,0 +1,114 @@
+package com.isvaso.serialization;
+
+import com.isvaso.exception.SerializerException;
+import com.isvaso.model.DataVersion;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataVersionSerializerTest {
+
+    private final DataVersionSerializer dataVersionSerializer = new DataVersionSerializer();
+
+    @Nested
+    class Serialize {
+
+        @Test
+        void shouldThrowSerializerException_whenDataVersionIsNull() {
+            DataVersion dataVersion = null;
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.serialize(dataVersion));
+        }
+
+        @Test
+        void shouldThrowSerializerException_whenVersionIsNegative() {
+            DataVersion dataVersion = new DataVersion(-1);
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.serialize(dataVersion));
+        }
+
+        @Test
+        void shouldSerialize_whenZeroVersion() throws SerializerException {
+            DataVersion dataVersion = new DataVersion(0);
+            String expectedVersion = "0";
+
+            String actualVersion = dataVersionSerializer.serialize(dataVersion);
+
+            assertEquals(expectedVersion, actualVersion);
+        }
+
+        @Test
+        void shouldSerialize_whenPositiveVersion() throws SerializerException {
+            DataVersion dataVersion = new DataVersion(1);
+            String expectedVersion = "1";
+
+            String actualVersion = dataVersionSerializer.serialize(dataVersion);
+
+            assertEquals(expectedVersion, actualVersion);
+        }
+    }
+
+    @Nested
+    class Deserialize {
+
+        @Test
+        void shouldThrowSerializerException_whenVersionIsNull() {
+            String dataVersion = null;
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.deserialize(dataVersion));
+        }
+
+        @Test
+        void shouldThrowSerializerException_whenVersionIsEmpty() {
+            String dataVersion = "";
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.deserialize(dataVersion));
+        }
+
+        @Test
+        void shouldThrowSerializerException_whenVersionIsBlank() {
+            String dataVersion = " ";
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.deserialize(dataVersion));
+        }
+
+        @Test
+        void shouldThrowSerializerException_whenNotANumberVersion() {
+            String dataVersion = "Version";
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.deserialize(dataVersion));
+        }
+
+        @Test
+        void shouldThrowSerializerException_whenNegativeVersion() {
+            String dataVersion = "-1";
+
+            assertThrows(SerializerException.class, () -> dataVersionSerializer.deserialize(dataVersion));
+        }
+
+        @Test
+        void shouldDeserialize_whenZeroVersion() throws SerializerException {
+            String dataVersion = "0";
+            int expectedVersion = 0;
+
+            Optional<DataVersion> dataVersionOptional = dataVersionSerializer.deserialize(dataVersion);
+
+            assertTrue(dataVersionOptional.isPresent());
+            assertEquals(expectedVersion, dataVersionOptional.get().getVersion());
+        }
+
+        @Test
+        void shouldDeserialize_whenPositiveVersion() throws SerializerException {
+            String dataVersion = "1";
+            int expectedVersion = 1;
+
+            Optional<DataVersion> dataVersionOptional = dataVersionSerializer.deserialize(dataVersion);
+
+            assertTrue(dataVersionOptional.isPresent());
+            assertEquals(expectedVersion, dataVersionOptional.get().getVersion());
+        }
+    }
+}
